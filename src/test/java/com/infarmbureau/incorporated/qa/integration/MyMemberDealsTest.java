763024942f0f963e9f1a30e467feb50b1f80826c
Sree Balakrishna <sree.balakrishna@infarmbureau.com>
2019-03-20 15:03:48 -0400

    Changes to the structure

--- src/test/java/com/infarmbureau/incorporated/qa/integration/MyMemberDealsTest.java
+++ src/test/java/com/infarmbureau/incorporated/qa/integration/MyMemberDealsTest.java
@@ -1,4 +1,4 @@
-package com.infarmbureau.incorporated.qa.integration.authentication;
+package com.infarmbureau.incorporated.qa.integration;
 
 import com.infarmbureau.incorporated.test.IfbIncorporatedTest;
 import com.infarmbureau.incorporated.test.pages.HomePage;
@@ -8,10 +8,10 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class    ResourcesTest  extends IfbIncorporatedTest {
+public class MyMemberDealsTest extends IfbIncorporatedTest {
 
     @Test
-    public void resourcesTest() {
+    public void myMemberDealsTest() {
         start();
         LoginPage loginPage = new LoginPage(driver);
         loginPage.usernameTextField.input("Markwetzel");
@@ -19,12 +19,12 @@ public class    ResourcesTest  extends IfbIncorporatedTest {
         loginPage.loginButton.safeClick();
 
         HomePage homePage = new HomePage(driver);
-        homePage.resourceslink.safeClick();
+        homePage.mymemberdealslink.safeClick();
 
         homePage.logoutlink.waitAndClick();
 
         LogoutPage logoutPage = new LogoutPage(driver);
-        assertEquals(logoutPage.logoutlabel.getText(),"Logout successful");
-
+        assertEquals(logoutPage.logoutlabel.getText(), "Logout successful");
     }
 }
+
--- src/test/java/com/infarmbureau/incorporated/qa/integration/ResourcesTest.java
+++ src/test/java/com/infarmbureau/incorporated/qa/integration/ResourcesTest.java
@@ -1,18 +1,17 @@
-package com.infarmbureau.incorporated.qa.integration.authentication;
+package com.infarmbureau.incorporated.qa.integration;
 
 import com.infarmbureau.incorporated.test.IfbIncorporatedTest;
 import com.infarmbureau.incorporated.test.pages.HomePage;
 import com.infarmbureau.incorporated.test.pages.LoginPage;
 import com.infarmbureau.incorporated.test.pages.LogoutPage;
-import com.infarmbureau.incorporated.test.pages.News.NewsPage;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class NewsTest extends IfbIncorporatedTest {
+public class    ResourcesTest  extends IfbIncorporatedTest {
 
     @Test
-    public void newsTest() {
+    public void resourcesTest() {
         start();
         LoginPage loginPage = new LoginPage(driver);
         loginPage.usernameTextField.input("Markwetzel");
@@ -20,25 +19,12 @@ public class NewsTest extends IfbIncorporatedTest {
         loginPage.loginButton.safeClick();
 
         HomePage homePage = new HomePage(driver);
-        homePage.newslink.safeClick();
-
-        NewsPage newsPage = new NewsPage(driver);
-
-        newsPage.ifbinthenewslink.safeClick();
-
-        newsPage.newsreleaseslink.safeClick();
-
-        newsPage.publicationslink.safeClick();
-
-
-        newsPage.mediacontactonformationlink.safeClick();
-
-        newsPage.socialmedialink.safeClick();
-
+        homePage.resourceslink.safeClick();
 
         homePage.logoutlink.waitAndClick();
 
         LogoutPage logoutPage = new LogoutPage(driver);
         assertEquals(logoutPage.logoutlabel.getText(),"Logout successful");
+
     }
 }
