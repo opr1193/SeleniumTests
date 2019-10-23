c208615ca60ff5d7529412cfaa35e8fbac59691b
Sree Balakrishna <sree.balakrishna@infarmbureau.com>
2019-10-23 12:22:23 -0400

    chn

--- src/main/java/com/infarmbureau/incorporated/test/pages/EventRegistration/EventRegistrationPage.java
+++ src/main/java/com/infarmbureau/incorporated/test/pages/EventRegistration/EventRegistrationPage.java
@@ -173,20 +173,8 @@ import static com.infarmbureau.qa.enums.Spinner.SPINNER;
 
         public IfbRadioButton tsessionthirdattendeegoingradiobutton = new IfbRadioButton(driver, tsessionthirdattendeegoingElement);
 
-
-
-
-
-
-
-
         @FindBy(xpath = "//*[@id=\"step5-form\"]/div[6]/div/div[1]/div[4]/h5[2]/span[1]/span")
-
-
-
-
         private WebElement paidfunsessionElement;//*[@id="step5-form"]/div[3]/div[1]/div[4]/h5[2]/div/span
-
 
         public IfbButton paidfunsessionbutton = new IfbButton(driver, paidfunsessionElement);
 
