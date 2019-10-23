4587b8ce67323fde736d22c27c2c47e35422faed
Sree Balakrishna <sree.balakrishna@infarmbureau.com>
2019-10-23 12:16:45 -0400

    chn

--- src/main/java/com/infarmbureau/incorporated/test/pages/EventRegistration/EventRegistrationPage.java
+++ src/main/java/com/infarmbureau/incorporated/test/pages/EventRegistration/EventRegistrationPage.java
@@ -174,22 +174,17 @@ import static com.infarmbureau.qa.enums.Spinner.SPINNER;
         public IfbRadioButton tsessionthirdattendeegoingradiobutton = new IfbRadioButton(driver, tsessionthirdattendeegoingElement);
 
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-        @FindBy(xpath = "//*[@id=\"step5-form\"]/div[6]/div/div[1]/div[4]/h5[2]/span[1]")
 
-        //*[@id="step5-form"]/div[6]/div/div[1]/div[4]/h5[2]/span[1]
-=======
-        @FindBy(xpath = "//*[@id=\"step5-form\"]/div[6]/div/div[1]/div[4]/h5[2]/span[1]/span")
->>>>>>> 3c4be504ba3e2e4b5bf8f590614397d0d511f8cc
-=======
-        @FindBy(xpath = "//*[@id=\"step5-form\"]/div[6]/div/div[1]/div[4]/h5[2]/span[1]/span")
-=======
-        @FindBy(xpath = "//*[@id=\"step5-form\"]/div[6]/div/div[1]/div[4]/h5[2]/span[1]")
 
-        //*[@id="step5-form"]/div[6]/div/div[1]/div[4]/h5[2]/span[1]
->>>>>>> master
->>>>>>> 791fac429c99950434d2910cc357781d99089e26
+
+
+
+
+        @FindBy(xpath = "//*[@id=\"step5-form\"]/div[6]/div/div[1]/div[4]/h5[2]/span[1]/span")
+
+
+
+
         private WebElement paidfunsessionElement;//*[@id="step5-form"]/div[3]/div[1]/div[4]/h5[2]/div/span
 
 
